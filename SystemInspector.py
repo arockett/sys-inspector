b34ce618271f7a50c9fd07e71c09534fe7274856
Aaron Beckett <becketta@msu.edu>
2013-06-10 09:32:16 -0400

    Added CLI_ARGS attribute

--- SystemInspector.py
+++ SystemInspector.py
@@ -14,11 +14,16 @@ import subprocess
 class SystemInspector:
     
     def __init__(self,extra_attributes=[]):
-        self.attr_names = ['PLATFORM','PYTHON_VERSION','INSTALLED_PYTHON_MODULES']
+        # Set-up dictionary of attributes
+        self.attr_names = []
         self.attributes = {}
+        # Add standard attributes
         self.add_attribute('PLATFORM', sys.platform)
         self.add_attribute('PYTHON_VERSION', sys.version)
         self.add_attribute('INSTALLED_PYTHON_MODULES', sys.modules.keys())
+        self.add_attribute('CLI_ARGS', ' '.join(sys.argv))
+        
+        # Version controll IDs
         if os.path.isdir('.git'):
             self.attr_names.append('GIT_HASH')
             git_hash = subprocess.check_output(['git', 'log', '--pretty=format:"%H"', '-n', '1'])
@@ -29,11 +34,14 @@ class SystemInspector:
         if os.path.isdir('.hg'):
             self.attr_names.append('MERCURIAL')
             self.add_attribute('MERCURIAL', True)
+            
+        # Add user provided attributes
         for attr in extra_attributes:
             self.attr_names.append(attr[0])
             self.add_attribute(attr[0],attr[1])
         
     def add_attribute(self,key,value):
+        self.attr_names.append(key)
         self.attributes[key] = value
         
     def append_attributes(self,extra_attributes):
